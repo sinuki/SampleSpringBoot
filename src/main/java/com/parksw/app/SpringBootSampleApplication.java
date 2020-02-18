48e0362147940af0a6f4a133452e59b42bf6fa9b
sinuki <parksw7302@gmail.com>
2020-02-18 22:59:15 +0900

    refactor. update sample

--- src/main/java/com/parksw/app/SpringBootSampleApplication.java
+++ src/main/java/com/parksw/app/SpringBootSampleApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootMvcApplication {
+public class SpringBootSampleApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootMvcApplication.class, args);
+		SpringApplication.run(SpringBootSampleApplication.class, args);
 	}
 }
